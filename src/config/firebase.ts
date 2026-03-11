c7a338fb390ef672e05e9e1bc8a65d55c61a0dcb
Sai-2211 <saipirathityan2211@gmail.com>
2026-03-12 02:12:57 +0530

    refactor: migrate Firebase integration to use React Native Firebase SDK.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,16 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDKiLZx-u1aSDqOIt7nm7Lpv15rBgOvhm8",
-  authDomain: "evcharger-437ad.firebaseapp.com",
-  projectId: "evcharger-437ad",
-  storageBucket: "evcharger-437ad.firebasestorage.app",
-  messagingSenderId: "553905450489",
-  appId: "1:553905450489:android:c9810bdc85a2ec44fa6128",
-};
-
-const app = initializeApp(firebaseConfig);
-export const firebaseAuth = getAuth(app);
-export const db = getFirestore(app);
+export const firebaseAuth = auth();
+export const db = firestore();
